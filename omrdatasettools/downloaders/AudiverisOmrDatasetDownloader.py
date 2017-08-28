f344964b897c4ae442d013673fd435ed8b1c67cf
Alexander Pacha <alexander.pacha@gmail.com>
2017-08-28 12:38:26 +0200

    Restoring autoclass and automethod calls that apparently don't work.

--- omrdatasettools/downloaders/AudiverisOmrDatasetDownloader.py
+++ omrdatasettools/downloaders/AudiverisOmrDatasetDownloader.py
@@ -8,7 +8,7 @@ class AudiverisOmrDatasetDownloader(DatasetDownloader):
     """ Loads and extracts the music symbols from the Audiveris OMR dataset.
         https://github.com/Audiveris/omr-dataset-tools
         Copyright 2017 by Hervé Bitteur under AGPL-3.0 license
-        """
+    """
 
     def __init__(self, destination_directory: str):
         """
