5b033f5b6958f2bb7b450b1fd52dd3efea873042
Alexander Pacha <alexander.pacha@gmail.com>
2019-02-08 12:02:56 +0100

    Removing Python 3.5 support, because we are using string-interpolation with f""-strings that are not supported in Python 3.5

--- setup.py
+++ setup.py
@@ -33,7 +33,6 @@ setup(
 
         # Specify the Python versions you support here.
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
     ],
