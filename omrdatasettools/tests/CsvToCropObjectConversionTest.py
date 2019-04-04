c4c7121c4b455c06869f6ff3355534a3b99d3c72
Alexander Pacha <alexander.pacha@gmail.com>
2019-04-04 17:37:29 +0200

    Fixing remaining unit-tests by setting path relative to current file's path.

--- omrdatasettools/tests/CsvToCropObjectConversionTest.py
+++ omrdatasettools/tests/CsvToCropObjectConversionTest.py
@@ -1,24 +1,28 @@
 import unittest
+import os
 
 from omrdatasettools.converters.csv_to_crop_object_conversion import convert_csv_annotations_to_cropobject
-from omrdatasettools.image_generators.ExportPath import ExportPath
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
 
 
 class CsvToCropObjectConversionTest(unittest.TestCase):
     def test_conversion_expect_number_of_objects(self):
         expected_number_of_crop_objects = 200
-        crop_objects = convert_csv_annotations_to_cropobject("testdata/CVC-MUSCIMA_W-01_N-10_D-ideal_1_detection.csv",
-                                                "testdata/CVC-MUSCIMA_W-01_N-10_D-ideal_1.png")
+        crop_objects = convert_csv_annotations_to_cropobject(
+            os.path.join(dir_path, "testdata", "CVC-MUSCIMA_W-01_N-10_D-ideal_1_detection.csv"),
+            os.path.join(dir_path, "testdata", "CVC-MUSCIMA_W-01_N-10_D-ideal_1.png"))
 
-        self.assertEquals(len(crop_objects), expected_number_of_crop_objects)
+        self.assertEqual(len(crop_objects), expected_number_of_crop_objects)
 
     def test_first_item_contains_correct_values(self):
         # Arrange
         # First line: CVC-MUSCIMA_W-01_N-10_D-ideal_1.png,138.93,2286.36,185.20,2316.52,8th_flag,1.00
 
         # Act
-        crop_objects = convert_csv_annotations_to_cropobject("testdata/CVC-MUSCIMA_W-01_N-10_D-ideal_1_detection.csv",
-                                                      "testdata/CVC-MUSCIMA_W-01_N-10_D-ideal_1.png")
+        crop_objects = convert_csv_annotations_to_cropobject(
+            os.path.join(dir_path, "testdata", "CVC-MUSCIMA_W-01_N-10_D-ideal_1_detection.csv"),
+            os.path.join(dir_path, "testdata", "CVC-MUSCIMA_W-01_N-10_D-ideal_1.png"))
 
         # Assert
         first_crop_object = crop_objects[0]
