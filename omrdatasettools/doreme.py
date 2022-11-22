285e490ecec6cc74ef18da605304f0f3c5297eb4
Elona Shatri <e.shatri1@gmail.com>
2022-11-22 11:17:35 +0000

    base

---
+++ omrdatasettools/doreme.py
@@ -0,0 +1,87 @@
+import argparse
+import os
+from glob import glob
+from typing import List
+
+from PIL import Image
+from mung.io import read_nodes_from_file
+from mung.node import Node
+from tqdm import tqdm
+
+from ExportPath import ExportPath
+
+
+class MuscimaPlusPlusSymbolImageGenerator:
+    def __init__(self) -> None:
+        super().__init__()
+        self.path_of_this_file = os.path.dirname(os.path.realpath(__file__))
+
+    def extract_and_render_all_symbol_masks(self, raw_data_directory: str, destination_directory: str):
+        """
+        Extracts all symbols from the raw XML documents and generates individual symbols from the masks
+
+        :param raw_data_directory: The directory, that contains the xml-files and matching images
+        :param destination_directory: The directory, in which the symbols should be generated into. One sub-folder per
+                                      symbol category will be generated automatically
+        """
+        print("Extracting Symbols from MUSCIMA++ Dataset...")
+
+        xml_files = self.get_all_xml_file_paths(raw_data_directory)
+        crop_objects = self.load_nodes_from_xml_files(xml_files)
+        self.render_masks_of_nodes_into_image(crop_objects, destination_directory)
+
+    def get_all_xml_file_paths(self, raw_data_directory: str) -> List[str]:
+        """ Loads all XML-files that are located in the folder.
+        :param raw_data_directory: Path to the raw directory, where the MUSCIMA++ dataset was extracted to
+        """
+        raw_data_directory = os.path.join(raw_data_directory, "v2.0", "data", "annotations")
+        xml_files = [y for x in os.walk(raw_data_directory) for y in glob(os.path.join(x[0], '*.xml'))]
+        return xml_files
+
+    def load_nodes_from_xml_files(self, xml_files: List[str]) -> List[Node]:
+        nodes = []  # type: List[Node]
+        for xml_file in tqdm(xml_files, desc="Loading nodes from xml-files", smoothing=0.1):
+            nodes.extend(read_nodes_from_file(xml_file))
+
+        print("Loaded {0} nodes".format(len(nodes)))
+        return nodes
+
+    def render_masks_of_nodes_into_image(self, nodes: List[Node], destination_directory: str):
+        for node in tqdm(nodes, desc="Generating images from node masks", smoothing=0.1):  # type: Node
+            symbol_class = node.class_name
+            # Make a copy of the mask to not temper with the original data
+            mask = node.mask.copy()
+            # We want to draw black symbols on white canvas. The mask encodes foreground pixels
+            # that we are interested in with a 1 and background pixels with a 0 and stores those values in
+            # an uint8 numpy array. To use Image.fromarray, we have to generate a greyscale mask, where
+            # white pixels have the value 255 and black pixels have the value 0. To achieve this, we simply
+            # subtract one from each uint, and by exploiting the underflow of the uint we get the following mapping:
+            # 0 (background) => 255 (white) and 1 (foreground) => 0 (black) which is exactly what we wanted.
+            mask -= 1
+            image = Image.fromarray(mask, mode="L")
+
+            target_directory = os.path.join(destination_directory, symbol_class)
+            os.makedirs(target_directory, exist_ok=True)
+
+            export_path = ExportPath(destination_directory, symbol_class, node.unique_id)
+            image.save(export_path.get_full_path())
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--raw_dataset_directory",
+        type=str,
+        default="/Users/elona/Documents/GitHub/dataloader/OMR-Datasets/omrdatasettools/raw_data",
+        help="The directory, where the Muscima++ dataset can be found. Must be at least version 2.0.")
+    parser.add_argument(
+        "--image_dataset_directory",
+        type=str,
+        default="/Users/elona/Documents/GitHub/dataloader/OMR-Datasets/omrdatasettools/raw_data/symbols",
+        help="The directory, where the generated bitmaps will be created")
+
+    flags, unparsed = parser.parse_known_args()
+
+    muscima_pp_image_generator = MuscimaPlusPlusSymbolImageGenerator()
+    muscima_pp_image_generator.extract_and_render_all_symbol_masks(flags.raw_dataset_directory,
+                                                                   flags.image_dataset_directory)
---
+++ omrdatasettools/doremi_mask.py
@@ -0,0 +1,204 @@
+import argparse
+import os
+from enum import Enum
+from glob import glob
+from typing import List, Tuple
+
+import numpy
+from PIL import Image
+from mung.io import read_nodes_from_file, parse_node_classes
+from mung.node import Node
+from tqdm import tqdm
+
+
+class MaskType(Enum):
+    """ The type of masks that should be generated """
+
+    #: creates mask images, where each type of node gets the same color mask (for semantic segmentation).
+    #: The classes staffLine, staff and staffSpace are ignored.
+    NODES_SEMANTIC_SEGMENTATION = 1
+
+    #: creates mask images, where the masks of the staff lines are contained for instance segmentation.
+    #: All five lines that form a staff will have the same color.
+    STAFF_LINES_INSTANCE_SEGMENTATION = 2
+
+    #: creates mask images, where each staff will receive one big blob (filling the staff space regions) per staff
+    #: line for instance segmentation. So each staff will have a different color.
+    STAFF_BLOBS_INSTANCE_SEGMENTATION = 3
+
+
+class MuscimaPlusPlusMaskImageGenerator:
+    def __init__(self) -> None:
+        self.path_of_this_file = os.path.dirname(os.path.realpath(__file__))
+        self.class_to_color_mapping = dict()
+
+    def render_node_masks(self, raw_data_directory: str, destination_directory: str, mask_type: MaskType):
+        """
+        Extracts all symbols from the raw XML documents and generates individual symbols from the masks
+
+        :param raw_data_directory: The directory, that contains the xml-files and matching images
+        :param destination_directory: The directory, in which the symbols should be generated into.
+                                      Per file, one mask will be generated.
+        :param mask_type: The type of masks that you want to generate, e.g., masks for each node or staff lines only.
+        """
+        print("Extracting Masks from Muscima++ Dataset...")
+
+        node_classes = parse_node_classes(
+            os.path.join(raw_data_directory,  "mff-muscima-mlclasses-annot.xml"))
+        for index, node_class in enumerate(node_classes):
+            self.class_to_color_mapping[node_class.name] = index + 1
+
+        file_paths = self.__get_all_file_paths(raw_data_directory)
+        for xml_file, png_file in tqdm(file_paths, desc="Generating mask images"):
+            original_image = Image.open(png_file)  # type: Image.Image
+            nodes = read_nodes_from_file(xml_file)
+            destination_filename = os.path.basename(xml_file).replace(".xml", ".png")
+            if mask_type == MaskType.NODES_SEMANTIC_SEGMENTATION:
+                self.__render_masks_of_nodes_for_semantic_segmentation(nodes, destination_directory,
+                                                                       destination_filename,
+                                                                       original_image.width, original_image.height)
+            if mask_type == MaskType.STAFF_LINES_INSTANCE_SEGMENTATION:
+                self.__render_masks_of_staff_lines_for_instance_segmentation(nodes, destination_directory,
+                                                                             destination_filename,
+                                                                             original_image.width,
+                                                                             original_image.height)
+            if mask_type == MaskType.STAFF_BLOBS_INSTANCE_SEGMENTATION:
+                self.__render_masks_of_staff_blob_for_instance_segmentation(nodes, destination_directory,
+                                                                            destination_filename,
+                                                                            original_image.width, original_image.height)
+            original_image.close()
+
+    def __get_all_file_paths(self, raw_data_directory: str) -> List[Tuple[str, str]]:
+        """ Loads all XML-files that are located in the folder.
+        :param raw_data_directory: Path to the raw directory, where the MUSCIMA++ dataset was extracted to
+        """
+        annotations_directory = os.path.join(raw_data_directory, "raw_data", "annotations")
+        xml_files = sorted([y for x in os.walk(annotations_directory) for y in glob(os.path.join(x[0], '*.xml'))])
+        images_directory = os.path.join(raw_data_directory, "raw_data", "images")
+        png_files = sorted([y for x in os.walk(images_directory) for y in glob(os.path.join(x[0], '*.png'))])
+        return list(zip(xml_files, png_files))
+
+    def __render_masks_of_nodes_for_semantic_segmentation(self, nodes: List[Node], destination_directory: str,
+                                                          destination_filename: str,
+                                                          width: int, height: int):
+        image = numpy.zeros((height, width), dtype=numpy.uint8)
+        skipped_classes = ["staffSpace", "staff", "staffLine"]
+        for node in reversed(nodes):
+            if node.class_name in skipped_classes:
+                continue
+            try:
+                symbol_class = node.class_name
+                color_mask = node.mask * self.class_to_color_mapping[symbol_class]
+                for i in range(node.height):
+                    for j in range(node.width):
+                        if color_mask[i, j] != 0:
+                            image[node.top + i, node.left + j] = color_mask[i, j]
+            except:
+                print("Error drawing node {0}".format(node.unique_id))
+
+        image = Image.fromarray(image, mode="L")
+        os.makedirs(destination_directory, exist_ok=True)
+        image.save(os.path.join(destination_directory, destination_filename))
+
+    def __render_masks_of_staff_lines_for_instance_segmentation(self, nodes: List[Node], destination_directory: str,
+                                                                destination_filename: str,
+                                                                width: int, height: int):
+        image = numpy.zeros((height, width), dtype=numpy.uint8)
+        included_classes = ["staffLine"]
+        staff_line_index = 0
+        staff_index = 1
+        for node in nodes:
+            if node.class_name not in included_classes:
+                continue
+            if staff_line_index == 5:
+                staff_line_index = 0
+                staff_index += 1
+            staff_line_index += 1
+            try:
+                color_mask = node.mask * staff_index
+                for i in range(node.height):
+                    for j in range(node.width):
+                        if color_mask[i, j] != 0:
+                            image[node.top + i, node.left + j] = color_mask[i, j]
+            except:
+                print("Error drawing node {0}".format(node.unique_id))
+
+        image = Image.fromarray(image, mode="L")
+        os.makedirs(destination_directory, exist_ok=True)
+        image.save(os.path.join(destination_directory, destination_filename))
+
+    def __render_masks_of_staff_blob_for_instance_segmentation(self, nodes: List[Node], destination_directory: str,
+                                                               destination_filename: str,
+                                                               width: int, height: int):
+        image = numpy.zeros((height, width), dtype=numpy.uint8)
+        included_classes = ["staffLine"]
+        staff_line_index = 0
+        staff_index = 1
+        for node in nodes:
+            if node.class_name not in included_classes:
+                continue
+
+            if staff_line_index == 4:
+                try:
+                    for i in range(first_staff_line_of_staff.top, node.bottom):
+                        for j in range(node.left, node.right):
+                            image[i, j] = staff_index
+                except:
+                    print("Error drawing node {0}".format(node.unique_id))
+
+            if staff_line_index == 5:
+                staff_index += 1
+                staff_line_index = 0
+
+            staff_line_index += 1
+            if staff_line_index == 1:
+                first_staff_line_of_staff = node
+
+        image = Image.fromarray(image, mode="L")
+        os.makedirs(destination_directory, exist_ok=True)
+        image.save(os.path.join(destination_directory, destination_filename))
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--raw_dataset_directory",
+        type=str,
+        default="/Users/elona/Documents/GitHub/dataloader/OMR-Datasets/omrdatasettools",
+        help="The directory, where the raw Muscima++ dataset can be found")
+    parser.add_argument(
+        "--image_dataset_directory",
+        type=str,
+        default="/Users/elona/Documents/GitHub/dataloader/OMR-Datasets/omrdatasettools/masks",
+        help="The directory, where the generated bitmaps will be created")
+    parser.add_argument(
+        "--mask_type",
+        type=str,
+        default="nodes_semantic",
+        help="One of the following types to be generated: [nodes_semantic, staff_lines, staff_blob]. Depending on the selected type, different"
+             "mask images will be created: "
+             "- nodes_semantic, creates mask images, where each type of node gets the same color mask (for semantic segmentation). The"
+             "classes staffLine, staff and staffSpace are ignored"
+             "- staff_lines, creates mask images, where the masks of the staff lines are contained for instance segmentation. All five "
+             "lines that form a staff will have the same color."
+             "- staff_blob, creates mask images, where each staff will receive one big blob (filling the staff space regions) per staff"
+             "line for instance segmentation. So each staff will have a different color.")
+
+    flags, unparsed = parser.parse_known_args()
+
+    if flags.mask_type not in ["nodes_semantic", "staff_lines", "staff_blob"]:
+        raise Exception(
+            "Invalid option for mask type selected. Must be one of [nodes_semantic, staff_lines, staff_blob], but was " + flags.mask_type)
+
+    if flags.mask_type == "nodes_semantic":
+        mask_type = MaskType.NODES_SEMANTIC_SEGMENTATION
+    if flags.mask_type == "staff_lines":
+        mask_type = MaskType.STAFF_LINES_INSTANCE_SEGMENTATION
+    if flags.mask_type == "staff_blob":
+        mask_type = MaskType.STAFF_BLOBS_INSTANCE_SEGMENTATION
+
+    mask_image_generator = MuscimaPlusPlusMaskImageGenerator()
+    # noinspection PyUnboundLocalVariable
+    mask_image_generator.render_node_masks(flags.raw_dataset_directory,
+                                           flags.image_dataset_directory,
+                                           mask_type)
